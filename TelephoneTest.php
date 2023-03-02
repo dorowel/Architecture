24275a68440aa56c5ad76d826371f3ecfa75ede0
dorowel <dorowel@gmail.com>
2023-03-02 16:17:47 -0300

    push project

---
+++ TelephoneTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Alura\Arquitetura;
+
+use PHPUnit\Framework\TestCase;
+
+class TelephoneTest extends TestCase
+{
+
+    public function testSetddd()
+    {
+        $a = 1;
+
+        $this->assertEquals(2,$a);
+    }
+}
